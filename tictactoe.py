638469f857ba1261241f1b7f0a63f0100eb67a42
murdskristians <murdskristians@gmail.com>
2020-11-20 18:53:03 +0200

    Made function for every field and player click option

--- tictactoe.py
+++ tictactoe.py
@@ -40,10 +40,85 @@ player = 1
 def activate(box):
     global player
     global bx1
+    global bx2
+    global bx3
+    global bx4
+    global bx5
+    global bx6
+    global bx7
+    global bx8
+    global bx9
     if box == 1 and player == 1:
         button1.config(text="O")
         player = 2
         bx1="O"
-
+    elif box == 1 and player == 2:
+        button1.config(text="X")
+        player =1
+        bx1="X"
+    elif box == 2 and player == 1:
+        button2.config(text="O")
+        player = 2
+        bx2="O"
+    elif box == 2 and player == 2:
+        button2.config(text="X")
+        player =1
+        bx2="X"
+    elif box == 3 and player == 1:
+        button3.config(text="O")
+        player = 2
+        bx3="O"
+    elif box == 3 and player == 2:
+        button3.config(text="X")
+        player =1
+        bx3="X"
+    elif box == 4 and player == 1:
+        button4.config(text="O")
+        player = 2
+        bx4="O"
+    elif box == 4 and player == 2:
+        button4.config(text="X")
+        player =1
+        bx4="X"
+    elif box == 5 and player == 1:
+        button5.config(text="O")
+        player = 2
+        bx5="O"
+    elif box == 5 and player == 2:
+        button5.config(text="X")
+        player =1
+        bx5="X"
+    elif box == 6 and player == 1:
+        button6.config(text="O")
+        player = 2
+        bx6="O"
+    elif box == 6 and player == 2:
+        button6.config(text="X")
+        player =1
+        bx6="X"
+    elif box == 7 and player == 1:
+        button7.config(text="O")
+        player = 2
+        bx7="O"
+    elif box == 7 and player == 2:
+        button7.config(text="X")
+        player =1
+        bx7="X"
+    elif box == 8 and player == 1:
+        button8.config(text="O")
+        player = 2
+        bx8="O"
+    elif box == 8 and player == 2:
+        button8.config(text="X")
+        player =1
+        bx8="X"
+    elif box == 9 and player == 1:
+        button9.config(text="O")
+        player = 2
+        bx9="O"
+    elif box == 9 and player == 2:
+        button9.config(text="X")
+        player =1
+        bx9="X"
 
 root.mainloop()
